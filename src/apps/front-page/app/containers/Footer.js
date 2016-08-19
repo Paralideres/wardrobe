8b97490b91e9e5328dd55e893870888147b77a88
Jorge Vivas <jorge@paralideres.org>
2016-08-19 16:11:32 -0700

    Front page styling work

---
+++ src/apps/front-page/app/containers/Footer.js
@@ -0,0 +1,111 @@
+import React from 'react';
+
+export default () => (
+  <div id="footer" className="clearfix">
+    <div id="nav_box" className="clearfix">
+      <div className="ft_title">
+      PUEDES VISITAR TAMBIÉN ...
+      </div>
+      <div className="ft_content">
+        <table >
+          <tr>
+            <td className="ft_sitemap_col clearfix">
+              <div id="ctl00_RecursosColumna1">
+                <ul>
+                  <li className="Titulo">
+                    <a href="#" onClick="IraSeccion(340)">
+                      Arte
+                    </a>
+                  </li>
+                  <li><a href="#" onClick="IraSeccion(33)">Cuentos y Poemas</a></li>
+                  <li><a href="#" onClick="IraSeccion(161)">Historietas</a></li>
+                  <li><a href="#" onClick="IraSeccion(32)">Guiones-Dramas</a></li>
+                </ul>
+                <ul>
+                  <li className="Titulo">
+                    <a href="#" onClick="IraSeccion(345)">
+                      El Camino
+                    </a>
+                  </li>
+                  <li><a href="#" onClick="IraSeccion(350)">El Camino: para Grupos Pequeños</a></li>
+                </ul>
+                <ul>
+                  <li className="Titulo">
+                    <a href="#" onClick="IraSeccion(169)">
+                      Plantillas
+                    </a>
+                  </li>
+                  <li><a href="#" onClick="IraSeccion(176)">Boletines</a></li>
+                </ul>
+              </div>
+            </td>
+            <td className="ft_sitemap_col clearfix">
+              <div id="ctl00_RecursosColumna2">
+                <ul>
+                  <li className="Titulo">
+                    <a href="#" onClick="IraSeccion(91)">
+                      Devocionales
+                    </a>
+                  </li>
+                  <li><a href="#" onClick="IraSeccion(111)">Sal y Luz</a></li>
+                  <li><a href="#" onClick="IraSeccion(112)">El Evangelio de Juan</a></li>
+                  <li><a href="#" onClick="IraSeccion(49)">Adviento</a></li>
+                  <li><a href="#" onClick="IraSeccion(166)">Pastoral para el Líder</a></li>
+                  <li><a href="#" onClick="IraSeccion(188)">Personajes Bíblicos y Mundo Postmoderno</a></li>
+                  <li><a href="#" onClick="IraSeccion(274)">Ellos se Encontraron con Jesús
+                    </a>
+                  </li>
+                  <li><a href="#" onClick="IraSeccion(354)">El Camino, la Verdad y la Vida, Yo Soy</a></li>
+                </ul>
+                <ul>
+                  <li className="Titulo">
+                    <a href="#" onClick="IraSeccion(67)">
+                      Estudios Bíblicos
+                    </a>
+                  </li>
+                  <li><a href="#" onClick="IraSeccion(31)">Grupos Pequeños o Discipulado</a></li>
+                  <li><a href="#" onClick="IraSeccion(113)">Estudios No Activos</a></li>
+                  <li><a href="#" onClick="IraSeccion(30)">Estudios Activos</a></li>
+                  <li><a href="#" onClick="IraSeccion(68)">Meditaciones</a></li>
+                  <li><a href="#" onClick="IraSeccion(174)">Herramientas De Estudio Para El Líder</a></li>
+                  <li><a href="#" onClick="IraSeccion(212)">Colecci&#243;n Pastoral Juvenil</a></li>
+                </ul>
+              </div>
+            </td>
+            <td className="ft_sitemap_col clearfix" >
+              <div id="ctl00_RecursosColumna3">
+                <ul>
+                  <li className="Titulo">
+                    <a href="#" onClick="IraSeccion(29)">
+                      Dinámicas/Juegos
+                    </a>
+                  </li>
+                  <li><a href="#" onClick="IraSeccion(57)">Exteriores - Grupos Grandes</a></li>
+                  <li><a href="#" onClick="IraSeccion(59)">Interiores - Activos</a></li>
+                  <li><a href="#" onClick="IraSeccion(128)">Actividades (varias)</a></li>
+                  <li><a href="#" onClick="IraSeccion(61)">Conociendo la Biblia</a></li>
+                  <li><a href="#" onClick="IraSeccion(58)">Exteriores - Grupos Pequeños</a></li>
+                  <li><a href="#" onClick="IraSeccion(63)">Rompehielos</a></li>
+                  <li><a href="#" onClick="IraSeccion(65)">Conociéndose Más</a></li>
+                  <li><a href="#" onClick="IraSeccion(127)">Rallys</a></li>
+                  <li><a href="#" onClick="IraSeccion(60)">Interiores - Tranquilos</a></li>
+                  <li><a href="#" onClick="IraSeccion(64)">Creando Compañerismo</a></li>
+                  <li><a href="#" onClick="IraSeccion(66)">Afirmándose</a></li>
+                </ul>
+                <ul>
+                  <li className="Titulo">
+                    <a href="#" onClick="IraSeccion(200)">Para Padres</a>
+                  </li>
+                </ul>
+              </div>
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
+
+    <div id="copy_box">
+      ParaLideres.org &copy; 2014 | <a href="#1">T&eacute;rminos</a> | <a href="#1">Privacidad</a> | Todos los derechos reservados
+    </div>
+  </div>
+);
